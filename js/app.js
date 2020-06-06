1fff0807e25dfc837e067e793965fc77d6996f95
adrianage <64158136+adrianage@users.noreply.github.com>
2020-06-06 11:39:55 -0700

    Added section 4

--- js/app.js
+++ js/app.js
@@ -21,8 +21,12 @@
 const nav = document.getElementById("navbar__list");
 // sections
 const sections = document.querySelectorAll("section");
+
+// Adding fourth section
+
+// let myDiv = '<div class="landing__container"></div>';
 /**
- * End Global Variables
+ * End Glo bal Variables
  * Start Helper Functions
  *
  */
@@ -46,14 +50,50 @@ function makeNav() {
 }
 
 makeNav();
+
+// aasddad
+
+const addElement = document.createElement("section");
+const addSection = document.querySelector("main");
+addElement.id = "section4";
+addElement.dataset.nav = "Section 4";
+
+addSection.insertAdjacentElement("beforeend", addElement);
+
+const divContainer = document.createElement("div");
+const addDiv = document.querySelector("#section4");
+divContainer.classList = "landing__container last-section";
+addDiv.insertAdjacentElement("afterbegin", divContainer);
+
+const sectionHeading = document.querySelector(".last-section");
+const addHeading = "<h2>Section 4</h2>";
+sectionHeading.insertAdjacentHTML("afterbegin", addHeading);
+
+const para = document.querySelector(".last-section");
+const paraText = document.createElement("p");
+paraText.textContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbifermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.";
+
+para.appendChild(paraText);
+
+const paraTwo = document.querySelector(".last-section");
+const paraTextTwo = document.createElement("p");
+paraTextTwo.textContent =
+  "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.";
+
+paraTwo.appendChild(paraTextTwo);
+
 // Add class 'active' to section when near top of viewport
 let offset = (section) => {
   return Math.floor(section.getBoundingClientRect().top);
 };
+// Remove class to non-active sections
 let removeClass = (section) => {
   section.classList.remove("your-active-class");
   section.style.cssText = "background-color: rgba(166, 42, 208, 0.1);";
 };
+
+// Add class to active sections
 
 let addClass = (conditional, section) => {
   if (conditional) {
@@ -61,7 +101,7 @@ let addClass = (conditional, section) => {
     section.style.cssText = "background-color: rgba(166, 42, 208, 0.5);";
   }
 };
-
+// Set sections as active
 function activeSection() {
   sections.forEach((section) => {
     const elementOffset = offset(section);
@@ -75,7 +115,8 @@ function activeSection() {
 window.addEventListener("scroll", activeSection);
 
 // Scroll to anchor ID using scrollTO event
-function toScroll() {
+// Scroll to section on link click
+function scroll() {
   const links = document.getElementsByClassName(".navbar__menu a");
   links.forEach((link) => {
     link.addEventListener("click", () => {
@@ -86,16 +127,10 @@ function toScroll() {
   });
 }
 
-toScroll();
+scroll();
 
 /**
  * End Main Functions
  * Begin Events
  *
  */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
