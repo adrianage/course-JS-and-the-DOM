6d90416744722ddf58b11405d064ee21862c01b3
adrianage <64158136+adrianage@users.noreply.github.com>
2020-06-07 21:10:58 -0700

    Removed code from li element and added comments

--- js/app.js
+++ js/app.js
@@ -34,13 +34,34 @@ const sections = document.querySelectorAll("section");
  *
  */
 
+//  Attempted to add fourth section through inserting HTML but being able to highligh Section 4 and have it show up on the navigation was a very difficult task. The elements for this section now reside on the HTML file of the project:
+
+// const addSection = document.querySelector("main");
+// const addElement = document.createElement("section");
+
+// addElement.id = "section4";
+// addElement.dataset.nav = "Section 4";
+
+// addSection.insertAdjacentElement(
+//   "beforeend",
+//   addElement
+// ).innerHTML += `<div class="landing__container">
+// <h2>Section 4</h2>
+// <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+// <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+// </div>
+// </section>`;
+
+// Removed this piece of code from the <li> element ${sectionID}
+
 // Making the navigation
 function makeNav() {
   let navItem = "";
   sections.forEach((section) => {
-    const sectionID = section.id;
+    // const sectionID = section.id;
+
     const sectionDataNav = section.dataset.nav;
-    navItem += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+    navItem += `<li><a class="menu__link" href="#">${sectionDataNav}</a></li>`;
   });
   nav.innerHTML = navItem;
 }
@@ -80,8 +101,9 @@ window.addEventListener("scroll", activeSection);
 
 // Scroll to anchor ID using scrollTO event
 // Scroll to section on link click
-function scroll() {
-  let hook = document.querySelectorAll(".navbar_menu a");
+
+const scroll = () => {
+  const hook = document.querySelectorAll(".navbar_menu a");
   hook.forEach((link) => {
     link.addEventListener("click", () => {
       for (i = 0; i < sections; i++) {
@@ -89,7 +111,7 @@ function scroll() {
       }
     });
   });
-}
+};
 
 scroll();
 
