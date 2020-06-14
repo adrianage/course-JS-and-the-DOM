63101cef1eef8b93d81375688bac91f39ed24eb6
adrianage <64158136+adrianage@users.noreply.github.com>
2020-06-14 04:07:30 -0700

    Updated js nav and some styling

--- js/app.js
+++ js/app.js
@@ -17,9 +17,9 @@
  * Define Global Variables
  *
  */
-
+// Navigation selector
 const nav = document.querySelector("#navbar__list");
-
+// Section selector
 const sections = document.querySelectorAll("section");
 
 /**
@@ -34,66 +34,60 @@ const sections = document.querySelectorAll("section");
  *
  */
 
-// build the nav
+// Build the nav
+
 const i = 0;
+
+// Elements for each section
 
 sections.forEach((section, i) => {
   let listItem = document.createElement("li");
   let link = document.createElement("a");
   let identSection = section.id;
-  let dataNavAtt = section.dataset.nav;
+  let dataNav = section.dataset.nav;
 
-  link.innerHTML += `${dataNavAtt}`;
+  // Additional HTML attributes
+  listItem.className = `item`;
+  link.innerHTML += `${dataNav}`;
   link.setAttribute("href", identSection);
   link.className += "menu__link";
 
+  // Appending children + items
   listItem.appendChild(link);
 
   nav.appendChild(listItem);
 
+  // Navigation listening to click event
+
   listItem.addEventListener("click", (event) => {
     event.preventDefault();
-    const sectionHook = event.target.getAttribute("a");
+    const linkTo = document.getElementById(`${identSection}`);
+    const sectionLink = linkTo.getAttribute("href");
 
-    const targetSection = document.querySelectorAll(".menu__link");
-    for (let j = 0; j < targetSection.length; j++) {
-      targetSection[j].addEventListener("click", (event) => {
-        event.preventDefault();
-        let hookOfSection = document.querySelectorAll("section");
-        hookOfSection.document.getAttribute("id");
-        hookOfSection.scrollIntoView({ behavior: "smooth" });
-      });
-    }
+    // Type of scrolling to section from clicking on the navbar
+
+    linkTo.scrollIntoView({ behavior: "smooth" });
   });
-});
 
-// Add class 'active' to section when near top of viewport
+  // Add class 'active' to section when near top of viewport
 
-function sectionView() {
-  for (let section of sections) {
-    const position = section.getBoundingClientRect();
-    if (position.top <= 150 && position.bottom >= 150) {
-      let id = section.getAttribute("id");
-      section.document
-        .querySelector(`.${id}`)
-        .classList.add("your-active-class");
-      section.classList.add("active");
-      section.style.cssText = "background-color: rgba(166, 42, 208, 0.1);";
-    } else {
-      let id = section.getAttribute("id");
-      section.document
-        .querySelector(`.${id}`)
-        .classList.remove("your-active-class");
-      section.classList.remove("active");
-      section.style.cssText = "background-color: rgba(166, 42, 208, 0.5);";
+  function sectionView() {
+    for (const section of sections) {
+      const position = section.getBoundingClientRect();
+      if (position.top <= 150 && position.bottom >= 150) {
+        section.classList.add("your-active-class");
+        section.classList.add("active");
+        section.style.cssText = "background-color: rgba(166, 42, 208, 0.5);";
+      } else {
+        section.classList.remove("your-active-class");
+        section.classList.remove("active");
+        section.style.cssText = "background-color: rgba(166, 42, 208, 0.1);";
+      }
     }
   }
-}
 
-window.document.addEventListener("scroll", function () {
-  sectionView();
+  document.addEventListener("scroll", sectionView);
 });
-
 // Scroll to anchor ID using scrollTO event
 
 /**
@@ -102,8 +96,17 @@ window.document.addEventListener("scroll", function () {
  *
  */
 
-// Build menu
-
-// Scroll to section on link click
-
 // Set sections as active
+
+const eagle = document.querySelector(".item");
+eagle.classList += " active";
+
+let bear = document.querySelector("ul");
+let tiger = document.querySelectorAll("li");
+tiger.forEach((el) => {
+  el.addEventListener("click", function () {
+    bear.querySelector(".active").classList.remove("active");
+
+    el.classList.add("active");
+  });
+});
